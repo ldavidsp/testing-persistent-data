edf12a7befb014fc0c9ea05022d3766d7a5f25d6
Luis Solorzano <luisnic20@gmail.com>
2019-03-12 11:21:09 -0600

    Data

---
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.testapplication
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import android.net.NetworkInfo
+import android.util.Log
+import android.net.ConnectivityManager
+import android.content.Intent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        registerReceiver(networkStateReceiver, IntentFilter(android.net.ConnectivityManager.CONNECTIVITY_ACTION))
+    }
+
+    public override fun onPause() {
+        unregisterReceiver(networkStateReceiver)
+        super.onPause()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private val networkStateReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val manager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val ni = manager.activeNetworkInfo
+            onNetworkChange(ni)
+        }
+    }
+
+
+    private fun onNetworkChange(networkInfo: NetworkInfo?) {
+        if (networkInfo != null) {
+            if (networkInfo.state == NetworkInfo.State.CONNECTED) {
+                Log.d("MainActivity", "CONNECTED")
+            } else {
+                Log.d("MenuActivity", "DISCONNECTED")
+            }
+        }
+    }
+}
